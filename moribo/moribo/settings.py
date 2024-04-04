98f7bd291aff2ecac94fe091d81a2d0e82ccd828
McKingz <king.judaah@gmail.com>
2024-04-04 19:31:12 +0200

    added routing to project

--- moribo/moribo/settings.py
+++ moribo/moribo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--w&ul%m4edpr7id$(iz(d_12cq$t#r+bpkmt25wexewne_r_!^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
