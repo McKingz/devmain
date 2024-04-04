053b85c7a3d0e3f2bcc6e962d8349605cf084655
king <king.judaah@gmail.com>
2024-04-04 21:00:47 +0200

    Update settings.py

--- moribo/moribo/settings.py
+++ moribo/moribo/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
